c4e4c52cf56b3deed4d2f21ce8d67b0d3358c5ba
tahir90 <tahir90.webdeveloper@gmail.com>
2025-10-12 13:49:44 +0400

    Updated index.html

--- src/main.js
+++ src/main.js
@@ -53,10 +53,25 @@ const setupContactForm = () => {
   }
 };
 
+const initMap = () => {
+  if (typeof L !== 'undefined') {
+    const map = L.map('map').setView([21.5169, 39.2192], 11);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([21.5169, 39.2192]).addTo(map)
+      .bindPopup('<b>Jeddah</b><br>Saudi Arabia')
+      .openPopup();
+  }
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
   await initI18n();
   updateContent();
   setupLanguageSwitcher();
   renderServices();
   setupContactForm();
+  initMap();
 });
