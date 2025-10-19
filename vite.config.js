a1da67c88673ba14dd465c9cdc894b12f52c686e
tahir90 <tahir90.webdeveloper@gmail.com>
2025-10-19 19:39:07 +0400

    Updated netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        blog: resolve(__dirname, 'blog.html')
+        blog: resolve(__dirname, 'blog.html'),
+        samsung: resolve(__dirname, 'samsung-washing-machine-repair.html'),
+        lg: resolve(__dirname, 'lg-washing-machine-repair.html'),
+        toshiba: resolve(__dirname, 'toshiba-washing-machine-repair.html'),
+        hitachi: resolve(__dirname, 'hitachi-washing-machine-repair.html'),
+        '404': resolve(__dirname, '404.html')
       }
     }
   }
